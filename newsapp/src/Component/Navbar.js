5a47cf7d7caa2f8eed70cce406b3e5f5287e8947
Vivek <86094260+Vivekyd6@users.noreply.github.com>
2021-12-18 10:43:28 +0530

    news app

---
+++ newsapp/src/Component/Navbar.js
@@ -0,0 +1,28 @@
+import React,{ Component } from "react";
+
+export default class Navbar extends Component {
+  render(){
+         return(
+             <div>
+             <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                 <a className="navbar-brand" href="/">News App</a>
+                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                     <span className="navbar-toggler-icon"></span>
+                 </button>
+
+                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul className="navbar-nav mr-auto">
+                         <li className="nav-item active">
+                             <a className="nav-link" href="/">Home</a>
+                         </li>
+                         <li className="nav-item">
+                             <a className="nav-link" href="/">About</a>
+                         </li>
+                     </ul>
+                 </div>
+             </nav>
+         </div>
+        )
+    }
+    
+};
